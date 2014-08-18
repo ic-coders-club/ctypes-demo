26311d4262c1db02dc957169525e7f518d505028
Keesjan de Vries <keesjan777@gmail.com>
2014-08-18 18:33:25 +0100

    simplify example

--- example1.c
+++ example1.c
@@ -1,16 +1,5 @@
-//modified from http://blog.prashanthellina.com/2008/01/07/interfacing-python-with-c-using-ctypes/
 
 int add(int a, int b)
 {
     return(a+b);
-}
-
-int sum_values(int *values, int n_values)
-{
-    int i;
-    int sum = 0;
-
-    for (i=0; i<n_values; i++){
-        sum+=values[i];
-    }
 }
